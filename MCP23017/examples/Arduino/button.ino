b63cff8b240fa6b6978d653f9a6969a8ed2dc3c0
Robert Eisele <robert@xarg.org>
2017-04-27 18:07:36 +0200

    added another mcp230xx example

---
+++ MCP23017/examples/Arduino/button.ino
@@ -0,0 +1,21 @@
+#include <Wire.h>
+#include "MCP23017.h"
+
+MCP23017 io;
+  
+void setup() {
+  
+  Wire.begin();
+
+  io.init();
+
+  io.setPinMode(6, INPUT);
+  io.setPullUpMode(6, HIGH);  // turn on 100K pullup internally
+
+  pinMode(13, OUTPUT);  // use the board LED for debugging
+}
+
+void loop() {
+  digitalWrite(13, io.getPin(6));
+  delay(10);
+}
