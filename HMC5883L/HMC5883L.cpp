8f5164c7693bf091d358b8dd6c178706bcc2afa3
Robert Eisele <robert@xarg.org>
2017-04-29 05:08:50 +0200

    added HMC5883L sensor

---
+++ HMC5883L/HMC5883L.cpp
@@ -0,0 +1,229 @@
+
+#include "HMC5883L.h"
+
+/**
+ * Set up compass for general usage with default settings
+ * 
+ */
+void HMC5883L::init() {
+
+#ifdef RASPBERRY
+    devId = i2cOpen(1, devId, 0);
+#endif
+
+    I2C::writeByte(devId, HMC5883L_CONFIG_A,
+            // Set number of samples averaged per measurement to 8
+            (HMC5883L_AVERAGING_8 << 5) |
+            // Set rate to 15 Hz
+            (HMC5883L_DATARATE_15HZ << 2) |
+            // Set normal measure bias
+            (HMC5883L_BIAS_NORMAL << 0));
+
+    // Set gain 
+    setGain(HMC5883L_GAIN_1090);
+
+    // Set single mode (which has very low power requirements)
+    setMeasureMode(HMC5883L_MODE_SINGLE);
+}
+
+// Config A Register
+
+void HMC5883L::setSampleAveraging(hmc5883l_averaging_t samples) {
+
+    // Set xx in 0xx11111
+
+    I2C::readByte(devId, HMC5883L_CONFIG_A, data);
+    I2C::writeByte(devId, HMC5883L_CONFIG_A, (samples << 5) | (*data & 31));
+}
+
+hmc5883l_averaging_t HMC5883L::getSampleAveraging() {
+
+    // Get xx in 0xx11111
+
+    I2C::readByte(devId, HMC5883L_CONFIG_A, data);
+
+    return (hmc5883l_averaging_t) ((*data >> 5) & 3);
+}
+
+/**
+ * Sets data rate for continues mode
+ * 
+ * @param dataRate
+ */
+void HMC5883L::setDataRate(hmc5883l_datarate_t rate) {
+
+    // Set xxx in 011xxx11
+
+    /*
+     * Value | Output Rate (Hz)
+     * ------+-----------------
+     * 0     | 0.75
+     * 1     | 1.5
+     * 2     | 3
+     * 3     | 7.5
+     * 4     | 15 (Default)
+     * 5     | 30
+     * 6     | 75
+     * 7     | Not used
+     */
+
+    I2C::readByte(devId, HMC5883L_CONFIG_A, data);
+    I2C::writeByte(devId, HMC5883L_CONFIG_A, (rate << 2) | (*data & 99));
+}
+
+hmc5883l_datarate_t HMC5883L::getDataRate() {
+
+    // Get xxx in 011xxx11
+
+    I2C::readByte(devId, HMC5883L_CONFIG_A, data);
+
+    return (hmc5883l_datarate_t) ((*data >> 2) & 7);
+}
+
+void HMC5883L::setMeasurementBias(hmc5883l_bias_t bias) {
+
+    // Set xx in 011111xx
+
+    I2C::readByte(devId, HMC5883L_CONFIG_A, data);
+    I2C::writeByte(devId, HMC5883L_CONFIG_A, bias | (*data & 124));
+}
+
+hmc5883l_bias_t HMC5883L::getMeasurementBias() {
+
+    // Get xx in 011111xx
+
+    I2C::readByte(devId, HMC5883L_CONFIG_A, data);
+
+    return (hmc5883l_bias_t) (*data & 3);
+}
+
+
+// Config B Register
+
+hmc5883l_gain_t HMC5883L::getGain() {
+
+    // Get xxx in xxx00000
+
+    I2C::readByte(devId, HMC5883L_CONFIG_B, data);
+
+    return (hmc5883l_gain_t) (*data >> 5);
+}
+
+/**
+ * Set magnetic field gain value
+ * 
+ * @param gain
+ */
+void HMC5883L::setGain(hmc5883l_gain_t gain) {
+    I2C::writeByte(devId, HMC5883L_CONFIG_B, gain << 5);
+
+    switch (gain) {
+        case HMC5883L_GAIN_1370:
+            scale = 1.0 / 1370 * HMC5883L_GAUSS_TO_MICROTESLA;
+            break;
+        case HMC5883L_GAIN_1090:
+            scale = 1.0 / 1090 * HMC5883L_GAUSS_TO_MICROTESLA;
+            break;
+        case HMC5883L_GAIN_820:
+            scale = 1.0 / 820 * HMC5883L_GAUSS_TO_MICROTESLA;
+            break;
+        case HMC5883L_GAIN_660:
+            scale = 1.0 / 660 * HMC5883L_GAUSS_TO_MICROTESLA;
+            break;
+        case HMC5883L_GAIN_440:
+            scale = 1.0 / 440 * HMC5883L_GAUSS_TO_MICROTESLA;
+            break;
+        case HMC5883L_GAIN_390:
+            scale = 1.0 / 390 * HMC5883L_GAUSS_TO_MICROTESLA;
+            break;
+        case HMC5883L_GAIN_330:
+            scale = 1.0 / 330 * HMC5883L_GAUSS_TO_MICROTESLA;
+            break;
+        case HMC5883L_GAIN_230:
+            scale = 1.0 / 230 * HMC5883L_GAUSS_TO_MICROTESLA;
+            break;
+    }
+}
+
+
+
+// MODE Register
+
+void HMC5883L::setMeasureMode(hmc5883l_mode_t newMode) {
+
+    // Set xx in 000000xx
+
+    mode = (uint8_t) newMode;
+
+    I2C::writeByte(devId, HMC5883L_MODE, mode);
+
+    if (newMode == HMC5883L_MODE_CONTINUOUS) {
+        I2C::sleep(6);
+    }
+}
+
+hmc5883l_mode_t HMC5883L::getMeasureMode() {
+
+    // Get xx in 000000xx
+
+    I2C::readByte(devId, HMC5883L_MODE, data);
+
+    return (hmc5883l_mode_t) (*data & 3);
+}
+
+
+// DATA Register
+
+/**
+ * Get the three axis heading measurement
+ * 
+ * @param x
+ * @param y
+ * @param z
+ */
+void HMC5883L::getMeasureRaw(int16_t *x, int16_t *y, int16_t *z) {
+
+    if (mode == HMC5883L_MODE_SINGLE) {
+        I2C::writeByte(devId, HMC5883L_MODE, HMC5883L_MODE_SINGLE);
+        I2C::sleep(6);
+    }
+
+    I2C::readBytes(devId, HMC5883L_DATA_START, data, 6);
+
+    *x = (((int16_t) data[0]) << 8) | data[1];
+    *z = (((int16_t) data[2]) << 8) | data[3];
+    *y = (((int16_t) data[4]) << 8) | data[5];
+}
+
+/**
+ * Gets normalized measure in uT
+ * 
+ * @param x
+ * @param y
+ * @param z
+ */
+void HMC5883L::getMeasure(float *x, float *y, float *z) {
+
+    int16_t _x, _y, _z;
+
+    getMeasureRaw(&_x, &_y, &_z);
+
+    *x = _x * scale;
+    *y = _y * scale;
+    *z = _z * scale;
+}
+
+// Ident Register
+
+/**
+ * Check if device is connected and ID register is readable
+ * 
+ * @return 
+ */
+bool HMC5883L::isAlive() {
+
+    if (I2C::readBytes(devId, HMC5883L_IDENT_A, data, 3) == 3) {
+        return data[0] == 0x48 && data[1] == 0x34 && data[2] == 0x33;
+    }
+    return false;
+}
---
+++ HMC5883L/HMC5883L.h
@@ -0,0 +1,130 @@
+
+#ifndef _HMC5883L_H
+#define _HMC5883L_H
+
+#include "I2C.h"
+
+#define HMC5883L_GAUSS_TO_MICROTESLA       100
+
+// Device Address, there is only one
+#define HMC5883L_ADDRESS            0x1E
+
+// Config register
+
+#define HMC5883L_CONFIG_A         0x00
+#define HMC5883L_CONFIG_B         0x01
+
+// Mode register
+#define HMC5883L_MODE             0x02
+
+// Data registers
+#define HMC5883L_DATA_MSB_X          0x03
+#define HMC5883L_DATA_LSB_X          0x04
+#define HMC5883L_DATA_MSB_Z          0x05
+#define HMC5883L_DATA_LSB_Z          0x06
+#define HMC5883L_DATA_MSB_Y          0x07
+#define HMC5883L_DATA_LSB_Y          0x08
+#define HMC5883L_DATA_START         HMC5883L_DATA_MSB_X
+
+// Status register
+#define HMC5883L_STATUS           0x09
+
+// Ident Registers
+#define HMC5883L_IDENT_A          0x0A
+#define HMC5883L_IDENT_B          0x0B
+#define HMC5883L_IDENT_C          0x0C
+
+typedef enum {
+    HMC5883L_AVERAGING_1 = 0x00,
+    HMC5883L_AVERAGING_2 = 0x01,
+    HMC5883L_AVERAGING_4 = 0x02,
+    HMC5883L_AVERAGING_8 = 0x03
+} hmc5883l_averaging_t;
+
+typedef enum {
+    HMC5883L_DATARATE_0_75_HZ = 0x00,
+    HMC5883L_DATARATE_1_5HZ = 0x01,
+    HMC5883L_DATARATE_3HZ = 0x02,
+    HMC5883L_DATARATE_7_5HZ = 0x03,
+    HMC5883L_DATARATE_15HZ = 0x04,
+    HMC5883L_DATARATE_30HZ = 0x05,
+    HMC5883L_DATARATE_75HZ = 0x06
+} hmc5883l_datarate_t;
+
+typedef enum {
+    HMC5883L_BIAS_NORMAL = 0x00,
+    HMC5883L_BIAS_POSITIVE = 0x01,
+    HMC5883L_BIAS_NEGATIVE = 0x02
+} hmc5883l_bias_t;
+
+typedef enum {
+    HMC5883L_GAIN_1370 = 0x00,
+    HMC5883L_GAIN_1090 = 0x01,
+    HMC5883L_GAIN_820 = 0x02,
+    HMC5883L_GAIN_660 = 0x03,
+    HMC5883L_GAIN_440 = 0x04,
+    HMC5883L_GAIN_390 = 0x05,
+    HMC5883L_GAIN_330 = 0x06,
+    HMC5883L_GAIN_230 = 0x07
+} hmc5883l_gain_t;
+
+typedef enum {
+    HMC5883L_MODE_CONTINUOUS = 0x00,
+    HMC5883L_MODE_SINGLE = 0x01,
+    HMC5883L_MODE_IDLE = 0x02
+} hmc5883l_mode_t;
+
+class HMC5883L {
+private:
+
+    uint8_t devId;
+    uint8_t data[6];
+    uint8_t mode;
+
+    float scale;
+
+public:
+
+    HMC5883L(uint8_t id) {
+        devId = id;
+    }
+
+    HMC5883L() {
+        devId = HMC5883L_ADDRESS;
+    }
+
+    void init();
+
+    bool isAlive();
+
+    // Config A Register
+
+    hmc5883l_averaging_t getSampleAveraging();
+    void setSampleAveraging(hmc5883l_averaging_t samples);
+
+    hmc5883l_datarate_t getDataRate();
+    void setDataRate(hmc5883l_datarate_t rate);
+
+    void setMeasurementBias(hmc5883l_bias_t bias);
+    hmc5883l_bias_t getMeasurementBias();
+
+    // Config B Register
+
+    hmc5883l_gain_t getGain();
+    void setGain(hmc5883l_gain_t gain);
+
+    // Mode Register
+
+    hmc5883l_mode_t getMeasureMode();
+    void setMeasureMode(hmc5883l_mode_t mode);
+
+    // Data Register
+
+    void getMeasureRaw(int16_t *x, int16_t *y, int16_t *z);
+
+    void getMeasure(float *x, float *y, float *z);
+
+    float getHeading();
+};
+
+#endif
