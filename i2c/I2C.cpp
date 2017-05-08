1959771e555f841eacadea290d5702b4f155c36a
Robert Eisele <robert@xarg.org>
2017-05-08 23:02:21 +0200

    Added i2c library

---
+++ i2c/I2C.cpp
@@ -0,0 +1,137 @@
+
+#include <I2C.h>
+
+void I2C::sleep(uint16_t d) {
+#ifdef ARDUINO
+    delay(d);
+#endif
+}
+
+bool I2C::readBit(uint8_t dev, uint8_t reg, uint8_t bit, uint8_t *data) {
+
+    uint8_t count = readByte(dev, reg, data);
+    *data &= 1 << bit;
+    return count == 1;
+}
+
+bool I2C::readByte(uint8_t dev, uint8_t reg, uint8_t *data) {
+
+    return readBytes(dev, reg, data, 1);
+}
+
+bool I2C::readBytes(uint8_t dev, uint8_t reg, uint8_t *data, uint8_t length) {
+
+    int8_t count = 0;
+
+    //if (length > BUFFER_LENGTH)
+    //    return 0;
+
+    Wire.beginTransmission(dev);
+    Wire.write(reg);
+    Wire.endTransmission();
+
+    Wire.requestFrom(dev, (uint8_t) length);
+
+    for (; Wire.available(); count++) {
+        data[count] = Wire.read();
+    }
+    return count == length;
+}
+
+bool I2C::readWord(uint8_t dev, uint8_t reg, uint16_t *data) {
+
+    return readWords(dev, reg, data, 1);
+}
+
+bool I2C::readWords(uint8_t dev, uint8_t reg, uint16_t *data, uint8_t length) {
+
+    uint8_t ndx = 1;
+    uint8_t count = 0;
+    uint8_t *ptr = (uint8_t *) data;
+
+    //if (length > BUFFER_LENGTH / 2)
+    //    return 0;
+
+    Wire.beginTransmission(dev);
+    Wire.write(reg);
+    Wire.endTransmission();
+
+    Wire.requestFrom(dev, (uint8_t) (length * 2));
+
+    while (Wire.available()) {
+
+        ptr[ndx] = Wire.read();
+        ndx = !ndx;
+
+        if (ndx) {
+            ptr += 2;
+            count++;
+        }
+    }
+    return count == length;
+}
+
+bool I2C::writeBit(uint8_t dev, uint8_t reg, uint8_t bit, bool data) {
+    uint8_t b;
+    readByte(dev, reg, &b);
+
+    // Conditional bit set
+    b ^= (-data ^ b) & (1 << bit);
+
+    return writeByte(dev, reg, b);
+}
+
+bool I2C::writeByte(uint8_t dev, uint8_t reg, uint8_t data) {
+    uint8_t status = 0;
+
+    Wire.beginTransmission(dev);
+    Wire.write(reg); // send address
+    Wire.write(data);
+    status = Wire.endTransmission();
+    return status == 0;
+}
+
+bool I2C::writeBytes(uint8_t dev, uint8_t reg, uint8_t* data, uint8_t length) {
+
+    uint8_t status = 0;
+    Wire.beginTransmission(dev);
+    Wire.write(reg); // send address
+
+    uint8_t* p = data;
+    uint8_t* e = data + length;
+
+    for (; p < e; p++) {
+        Wire.write(p[0]);
+    }
+    status = Wire.endTransmission();
+    return status == 0;
+}
+
+bool I2C::writeWord(uint8_t dev, uint8_t reg, uint16_t data) {
+
+    uint8_t status = 0;
+
+    Wire.beginTransmission(dev);
+    Wire.write(reg);
+    Wire.write(((uint8_t *) data)[1]); // send MSB
+    Wire.write(((uint8_t *) data)[0]); // send LSB
+    status = Wire.endTransmission();
+    return status == 0;
+}
+
+bool I2C::writeWords(uint8_t dev, uint8_t reg, uint16_t* data, uint8_t length) {
+
+    uint8_t status = 0;
+    Wire.beginTransmission(dev);
+    Wire.write(reg); // send address
+
+    uint8_t* p = (uint8_t *) data;
+    uint8_t* e = (uint8_t *) data + 2 * length;
+
+    for (; p < e; p += 2) {
+        Wire.write(p[1]); // send MSB
+        Wire.write(p[0]); // send LSB
+    }
+    status = Wire.endTransmission();
+    return status == 0;
+}
---
+++ i2c/I2C.h
@@ -0,0 +1,50 @@
+
+
+#ifndef _I2C_H
+#define _I2C_H
+
+#ifdef ARDUINO
+#if ARDUINO < 100
+#include "WProgram.h"
+#else
+#include "Arduino.h"
+#endif
+
+#ifdef __AVR_ATtiny85__
+#include <TinyWireM.h>
+#else
+#include <Wire.h>
+#endif
+#endif
+
+#ifdef __AVR
+#include <avr/pgmspace.h>
+#elif defined(ESP8266)
+#include <pgmspace.h>
+#endif
+
+#define GET_BITS_FROM_BYTE(x, a, b) (((x) >> (a)) & ((1 << ((b) - (a) + 1)) - 1))
+#define SET_BITS_IN_BYTE(x, a, b, v) (((x) & ~(((1 << ((b) - (a) + 1)) - 1) << (a))) | (v << (a)))
+
+class I2C {
+public:
+
+    I2C() {
+    }
+
+    static void sleep(uint16_t delay);
+
+    static bool readBit(uint8_t dev, uint8_t reg, uint8_t bit, uint8_t *data);
+    static bool readByte(uint8_t dev, uint8_t reg, uint8_t *data);
+    static bool readBytes(uint8_t dev, uint8_t reg, uint8_t *data, uint8_t length);
+    static bool readWord(uint8_t dev, uint8_t reg, uint16_t *data);
+    static bool readWords(uint8_t dev, uint8_t reg, uint16_t *data, uint8_t length);
+
+    static bool writeBit(uint8_t dev, uint8_t reg, uint8_t bit, bool data);
+    static bool writeByte(uint8_t dev, uint8_t reg, uint8_t data);
+    static bool writeBytes(uint8_t dev, uint8_t reg, uint8_t *data, uint8_t length);
+    static bool writeWord(uint8_t dev, uint8_t reg, uint16_t data);
+    static bool writeWords(uint8_t dev, uint8_t reg, uint16_t *data, uint8_t length);
+};
+
+#endif
